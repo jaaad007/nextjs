162df1de6ffb3d62011e38b85427b35035c3e5d0
jad rafalia <jadrafalia15@gmail.com>
2025-06-24 11:56:25 +0200

    Ajout message personnalisé

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      hello jad
-    </div>
+    <main style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center', fontSize: '24px' }}>
+      👋 Bienvenue sur mon site Next.js déployé avec Vercel
+    </main>
   );
 }
