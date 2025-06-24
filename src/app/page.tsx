63247f5cbf4a349ddf89fa9dc0361fdb76086765
jad rafalia <jadrafalia15@gmail.com>
2025-06-24 12:08:15 +0200

    Ajout message personnalisé

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center', fontSize: '24px' }}>
-      👋 Bienvenue Rafalia Jad
+      Welcome
     </main>
   );
 }
