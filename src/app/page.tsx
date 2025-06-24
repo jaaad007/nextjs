c97ff6f57d9e4f076486ce00d09f7e1b592aee98
jad rafalia <jadrafalia15@gmail.com>
2025-06-24 12:02:27 +0200

    Ajout message personnalisé

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center', fontSize: '24px' }}>
-      👋 Bienvenue sur mon site Next.js déployé avec Vercel
+      👋 Bienvenue Jad
     </main>
   );
 }
